c39df4f6666e49b5d87c9ed746b3bf9511b4745e
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-12 16:48:36 +0200

    add comment + indentation

--- js/script_evenOdd.js
+++ js/script_evenOdd.js
@@ -12,38 +12,39 @@ var user = document.getElementById("even-odd");
 var userNumber = document.getElementById("number");
 var btn = document.getElementById("btn-play");
 var result = document.getElementById("result");
-var btnReset=document.getElementById("btn-reset");
-var itemResult=document.getElementById("item-result");
+var btnReset = document.getElementById("btn-reset");
+var itemResult = document.getElementById("item-result");
 
 btn.addEventListener("click", function () {
-  
+
     validateNumber(parseInt(userNumber.value));
     var sum = 0;
     var msg = "";
     //*? 2: invoco la funzione random per in numero del computer
-    var rand = randomNumber(1,5);
-    console.log(rand);  
+    var rand = randomNumber(1, 5);
+    console.log(rand);
     //*? 3: sommiano i due numeri;
     sum = rand + parseInt(userNumber.value);
-   
+
     //*? 4: test vincita
     if ((isEven(sum) && (user.value === "even")) || (!isEven(sum) && (user.value === "odd"))) {
         msg = "Hai vinto : la somma è " + sum;
-        result.style.color="#ff4";
+        result.style.color = "#ff4";
     } else {
         msg = "Hai perso : la somma è " + sum;
-        result.style.color="#f00";
+        result.style.color = "#f00";
     }
     //? 5: Output text html
     itemResult.classList.remove("hidden");
-    result.innerText=msg;
+    result.innerText = msg;
     btn.classList.add("disabled");
 });
 
-btnReset.addEventListener("click",function(){
-    user.value="even";
-    userNumber.value="1";
-    itemResult.classList.add("hidden"); 
+//?BONUS: inserimento bottone reset
+btnReset.addEventListener("click", function () {
+    user.value = "even";
+    userNumber.value = "1";
+    itemResult.classList.add("hidden");
     console.clear();
     btn.classList.remove("disabled");
 });
@@ -52,13 +53,13 @@ btnReset.addEventListener("click",function(){
  * @param {number} num 
  * @returns  alert se non hai inserito un numero corretto
  */
- function validateNumber(num){
-    while(isNaN(num) || (num<=0 || num>=6)){
+function validateNumber(num) {
+    while (isNaN(num) || (num <= 0 || num >= 6)) {
         alert("hai inserito un numero non valido.")
         return;
-     }
- }
-/**
+    }
+}
+/** testa se è pari o dispari
  * 
  * @param {number} num 
  * @returns {boolean} se è pari vero altrimenti falso
@@ -75,4 +76,3 @@ function randomNumber(min, max) {
     max++;
     return (Math.floor(Math.random() * (max - min)) + min);
 }
- 
